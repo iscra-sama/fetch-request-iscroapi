2829a4e5ae5ea60baadeec992121aef9de88ef2b
nyaascorka <nyaascorka@gmail.com>
2026-02-21 15:26:55 +0000

    upd

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ def fetch_and_response():
         response = requests.get(request.headers.get("Origin"))
         response.raise_for_status()
     except (HTTPError, ConnectionError, Timeout, RequestException) as err:
+        # DEBUG:
+        print(err)
         return (err.__str__(), 500, {"Content-Type": "text/plain"})
     return ("Hello, Iscra-chan", {"Content-Type": "text/plain"})
 
