79178736510ee53fd38ddc0cdb62c575f94719f4
nyaascorka <nyaascorka@gmail.com>
2026-02-21 16:35:14 +0000

    upd

--- main.py
+++ main.py
@@ -15,9 +15,7 @@ def index1(path):
 def fetch_and_response():
     from requests.exceptions import RequestException, HTTPError, ConnectionError, Timeout
 
-    logging.info(request.data)
     url_to_fetch = request.get_json(silent=True)
-    logging.info(url_to_fetch)
     if url_to_fetch is None:
         return (jsonify("Bad JSON"), 400)
     try:
