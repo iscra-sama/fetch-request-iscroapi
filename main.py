9d462931fd6027ed3860d991c731f0135287b5d0
nyaascorka <nyaascorka@gmail.com>
2026-02-21 14:52:06 +0000

    upd

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+@app.route("/<path:path>")
+def index1(path):
+    return "Hello, Iscra-chan!"
+
 @app.route("/request-fetch", methods=["POST"])
 def fetch_and_response():
     from requests.exceptions import RequestException, HTTPError, ConnectionError, Timeout
@@ -15,11 +19,6 @@ def fetch_and_response():
     except (HTTPError, ConnectionError, Timeout, RequestException) as err:
         return (err.__str__(), 500, {"Content-Type": "text/plain"})
     return ("Hello, Iscra-chan", {"Content-Type": "text/plain"})
-
-@app.route("/", defaults={'path': ''})
-@app.route("/<path:path>")
-def index(path):
-    return "Hello, Iscra-chan!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8000)))
