bba7874ca606d330ed755bf5e4324061b23a4082
nyaascorka <nyaascorka@gmail.com>
2026-02-21 13:03:05 +0000

    upd

--- main.py
+++ main.py
@@ -17,5 +17,5 @@ def index(_):
     return "Hello, Iscra-chan!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 443)))
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8000)))
 
